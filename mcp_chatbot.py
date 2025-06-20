0cc53a3101cf7538b8011b73efcccf0f2fc17945
Shikhar Dave <b22ch032@iitj.ac.in>
2025-06-20 10:49:36 +0530

    basic_files

--- mcp_chatbot.py
+++ mcp_chatbot.py
@@ -9,8 +9,22 @@ from google.genai import types
 
 nest_asyncio.apply()
 
+client = genai.Client(api_key="")
+
 class MCP_Chatbot:
     def __init__(self):
         self.available_tools: List[dict] = []
         self.session: ClientSession = None
+        
+    async def process_query(self, query):
+        messages = [{'role':'user', 'content':query}]
+        response = client.models.generate_content(
+                model="gemini-2.5-pro-exp-03-25",
+                contents=prompt,
+                config=types.GenerateContentConfig(
+                    temperature=0,
+                    tools=available_tools,
+                ),
+            )
+        process_query = True
     
