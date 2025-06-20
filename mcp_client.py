2716f2a4c028fbc42513702f4e971975f09cae66
Shikhar Dave <b22ch032@iitj.ac.in>
2025-06-20 08:39:36 +0530

    server_params

--- mcp_client.py
+++ mcp_client.py
@@ -1,1 +1,9 @@
 from mcp import ClientSession, types, StdioServerParameters 
+from mcp.client.stdio import stdio_client
+
+server_params = StdioServerParameters(
+    command="uv",  # Executable
+    args=["run example_server.py"],  # Command line arguments
+    env=None,  # Optional environment variables
+)
+
