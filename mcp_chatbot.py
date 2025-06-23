02e7187a14ebb5d4564ef0c2f23dd99cdc24eae9
Shikhar Dave <b22ch032@iitj.ac.in>
2025-06-23 08:44:09 +0530

    connect_server

--- mcp_chatbot.py
+++ mcp_chatbot.py
@@ -81,3 +81,30 @@ class MCP_Chatbot:
                     
             except Exception as e:
                 print(f"\nError: {str(e)}")
+                
+    async def connect_to_server_and_run(self):
+        # Create server parameters for stdio connection
+        server_params = StdioServerParameters(
+            command="uv",  # Executable
+            args=["run", "research_server.py"],  # Optional command line arguments
+            env=None,  # Optional environment variables
+        )
+        async with stdio_client(server_params) as (read, write):
+            async with ClientSession(read, write) as session:
+                self.session = session
+                # Initialize the connection
+                await session.initialize()
+    
+                # List available tools
+                response = await session.list_tools()
+                
+                tools = response.tools
+                print("\nConnected to server with tools:", [tool.name for tool in tools])
+                
+                self.available_tools = [{
+                    "name": tool.name,
+                    "description": tool.description,
+                    "input_schema": tool.inputSchema
+                } for tool in response.tools]
+    
+                await self.chat_loop()
