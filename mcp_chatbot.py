3c0fa84361ad17e3ffda12aefc3ba445f3ef7b60
Shikhar Dave <b22ch032@iitj.ac.in>
2025-06-26 10:57:45 +0530

    tool_definition_added

--- mcp_chatbot.py
+++ mcp_chatbot.py
@@ -1,5 +1,5 @@
 from mcp.client.stdio import stdio_client
-from typing import List 
+from typing import List, TypedDict , Dict
 import asyncio
 import nest_asyncio
 from mcp import StdioServerParameters,types, ClientSession
@@ -7,10 +7,16 @@ from dotenv import load_dotenv
 from google import genai 
 from google.genai import types 
 import os
+from contextlib import AsyncExitStack
+import json
 from google.generativeai.types import GenerateContentConfig
-nest_asyncio.apply()
 load_dotenv()
 genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+
+class ToolDefinition(TypedDict):
+    name: str
+    description: str
+    input_schema: dict
 
 class MCP_Chatbot:
     def __init__(self):
